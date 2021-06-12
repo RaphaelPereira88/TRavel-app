2cef2e79da2464b1b4701d58afbfd62bb9463f18
Raphael Pereira <raphael.pereira88@gmail.com>
2021-06-12 10:16:17 +0100

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const app = require("./index.js");
+app.set('port', process.env.PORT || 8081);
 
-app.listen(8081, function () {
-  console.log(`running on localhost: 8081`);
+app.listen(app.get('port'), function () {
+  console.log(`running on localhost: `, server.address().port);
 });
